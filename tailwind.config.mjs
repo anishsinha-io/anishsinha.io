2c9f4626cc7f1bb1a8ba6fb9c4587c0d719eb867
Anish Sinha <anishsinha0128@gmail.com>
2024-08-22 21:47:20 -0400

    Update relative import paths to use ~ imports and remove an unnecessary color from tailwind configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -21,9 +21,7 @@ export default {
         offwhite: "#f9f9f9",
         mediumgray: "#5C5D67",
         midnight: "#121214",
-        space: "#061826",
       },
     },
   },
-  // plugins: [],
 };
